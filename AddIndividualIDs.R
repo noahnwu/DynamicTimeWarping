c87530fab0775566702e80300d029e1684bb5d02
Noah James Forrest <noah.forrest@northwestern.edu>
2024-07-11 12:09:11 -0500

    Created Function For Adding Patient IDs

---
+++ AddIndividualIDs.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+AddIndividualIDs = function(SequenceList, 
+                            GroupName, 
+                            TimeColumn = "TimePoint") { 
+  #Function that Concatenates Raw Cachexia 
+  #Measure Sequences into a single dataframe
+  #And adds patient ids
+  return(
+    SequenceList %>%  
+      reduce(., bind_rows) %>%  
+      mutate(PatientID = paste0(GroupName, "_", cumsum(!!sym(TimeColumn) == 1))) %>%  
+      relocate(PatientID)
+  )
+  
+}
+
+test = AddIndividualIDs(Mild, "Mild")
